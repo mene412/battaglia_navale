e128e6900ee6d9409249b225fbc5b05a68dbcf02
Araglas34 <121188523+Araglas34@users.noreply.github.com>
2022-12-29 10:41:15 +0000

    Update Battleship.h

--- include/Battleship.h
+++ include/Battleship.h
@@ -1,1 +1,16 @@
 // Autore:
+#ifndef Battleship_h
+#define Battleship_h
+#include "Ship.h"
+class Corazzata: public Ship
+{
+public:
+    Corazzata()
+    {
+        type='C';
+        dim=5;
+        armor=5;
+        set_coord();
+    }
+};
+#endif /* Battleship.h */
