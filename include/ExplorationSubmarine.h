0639fbf22f5144ee80d0a07bad6536b8d3ded5bd
Araglas34 <121188523+Araglas34@users.noreply.github.com>
2022-12-29 10:39:10 +0000

    Update ExplorationSubmarine.h

--- include/ExplorationSubmarine.h
+++ include/ExplorationSubmarine.h
@@ -1,1 +1,15 @@
 // Autore:
+#ifndef ExplorationSubmarine_h
+#define ExplorationSubmarine_h
+#include "Ship.h"
+class Submarine: public Ship
+{
+public:
+    Submarine()
+    {
+        type='E';
+        dim=1;
+        armor=1;
+        set_coord();
+    }
+ #endif /* ExplorationSubmarine.h */
