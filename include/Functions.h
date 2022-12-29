51ce6d7c1de59c957f70e2219824183f95385ea8
Araglas34 <wwfgaia@gmail.com>
2022-12-29 12:54:36 +0100

    Change 1
    Diviso Function in .h e .cpp

--- include/Functions.h
+++ include/Functions.h
@@ -1,19 +1,6 @@
 
 #ifndef Functions_h
 #define Functions_h
-#include <vector>
-using namespace std;
-char num_to_lett(int t)
-{
-    vector<char> letters={'a','b','c','d','e','f','g','h','i','l','m','n'};
-    return letters[t-1];
-};
-int char_to_int(char t)
-{
-   vector<char> letters={'a','b','c','d','e','f','g','h','i','l','m','n'};
-    int s=0;
-    while(letters[s]!=t)
-        s++;
-    return s+1;
-};
+char num_to_lett(int t);
+int char_to_int(char t);
 #endif /* Functions_h */
---
+++ src/Functions.cpp
@@ -0,0 +1,15 @@
+#include "Functions.h"
+using namepsace std;
+char num_to_lett(int t)
+{
+    vector<char> letters={'a','b','c','d','e','f','g','h','i','l','m','n'};
+    return letters[t-1];
+};
+int char_to_int(char t)
+{
+   vector<char> letters={'a','b','c','d','e','f','g','h','i','l','m','n'};
+    int s=0;
+    while(letters[s]!=t)
+        s++;
+    return s+1;
+};
