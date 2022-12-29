758e2cb6facc5dba0af49cea33f1d1010867e623
Araglas34 <121188523+Araglas34@users.noreply.github.com>
2022-12-29 10:43:24 +0000

    Update HelpShip.h

--- include/HelpShip.h
+++ include/HelpShip.h
@@ -1,1 +1,16 @@
 // Autore:
+#ifndef HelpShip_h
+#define HelpShip_h
+#include "Ship.h"
+class Support_Ship: public Ship
+{
+public:
+    Support_Ship()
+    {
+        type='S';
+        dim=3;
+        armor=3;
+        set_coord();
+    }
+};
+#endif /* HelpShip_h */
