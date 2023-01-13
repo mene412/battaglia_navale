3e319d702693c6c2b8afd500244ea6840e2763c4
mene412 <matteomene02@gmail.com>
2023-01-13 21:22:35 +0100

    Modifica move helpShip

--- include/HelpShip.h
+++ include/HelpShip.h
@@ -7,8 +7,7 @@
 class HelpShip : public Ship {
     public:
         HelpShip(Coord front, Coord back);
-        // controlla se le celle sono vuote, se true allora
-        // cancella quelle vecchie, scrive quelle nuove e modifica i membri della ship
+        // modifica i membri della ship
         void move(Coord c);
         // se trova nella griglia 3x3 una corazzata o una helpship, e manca vita
         // allora la cura del tutto
