5572ed0b728a6428056c0b330ba0b9c8f229dd92
andreamu02 <118268232+andreamu02@users.noreply.github.com>
2023-01-13 21:46:03 +0100

    Backup

--- include/Ship.h
+++ include/Ship.h
@@ -6,36 +6,35 @@
 class Ship
 {
     public:
-        Ship(Coord front, Coord back);
-        char type(void) { return type_; }
-        void set_coord_center(Coord cord);
-        void set_center(void);
         void set_direction(void);
-        std::vector<Coord> const coord(void) { return coord_; }
+        void set_center(void);
+        void set_coord_center(Coord cord);
+        void set_coord(std::vector<Coord>& coordinates);
+        std::vector<Coord>& coord(void){ return coord_; }
+        std::vector<int>& coord_hit(void){ return coord_hit_;}
+        Coord const coord(int a){return coord_[a];}
         int const dim(void) { return dim_; }
         int armor(void) { return armor_; }
-        int x(void) { return x_; }
-        int y(void) { return y_; }
         void set_healed(bool a);
-        void set_coord(std::vector<Coord> coord);
         bool healed(void) { return healed_; }
-        bool ifSameCoord(int x, int y);
-        void incArmor(void);
-        void decArmor(void);
         Coord center(void){ return center_; }
         bool left(void) { return left_; }
+        void dec_armor();
         bool orizzontal(void) { return orizzontal_; }
-        virtual ~Ship() {};
+        void set_armor(int a);
+        virtual ~Ship(){};
+        void heal(void);
+        void hit(Coord c);
+
     protected:
+        Ship(Coord front, Coord back);
         std::vector<Coord> coord_;
+        std::vector<int> coord_hit_;
         int dim_;
         int armor_;
-        int num_;
-        int x_;     // numero riga (vedi pg. 3 progetto Tonin)
-        int y_;     // numero colonna
-        char type_;
         bool healed_;
         int distance_;
+
     private:
         Coord front_;
         Coord back_;
