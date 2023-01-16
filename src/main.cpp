5477c1f1f6a5dcd699230f846e087a54fa30e1d5
Araglas34 <wwfgaia@gmail.com>
2023-01-16 09:12:23 +0100

    Aggiuto main con command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "Coord.cpp"
 #include "AttackGrid.cpp"
 #include "Battleship.cpp"
-#include "DefenceGrid.cpp"
+#include "DefenceGrid.cpp"              //non dovrebbero essere .h gli include?
 #include "ExplorationSubmarine.cpp"
 #include "Game.cpp"
 #include "Grid.cpp"
@@ -17,3 +17,26 @@ int main(int argc, char* argv[]) {
     game.start();
     return 0;
 }
+//MAIN CON COMMAND LINE ARGUMENTS
+
+//int main(int argc, char* argv[]) {
+//    std::srand(time(NULL));
+//    if(argc>1){
+//        if(!strcmp(argv[1], "pc")){
+//            GamePlayer game{};
+//            game.start();
+//        }
+//        else if(!strcmp(argv[1], "cc")){
+ //           GamePc game{};
+//            game.start();
+//        }
+//        else{
+//            std::cout<<"Invalid argument"<<std::endl;
+//        }
+//    }
+//    else{
+//        std::cout<<"Command line argument required"<<std::endl;
+//    }
+    
+//    return 0;
+//}
