d48c2fcf0bf40e96619b91e9ce7ab40aedec55a1
mene412 <matteomene02@gmail.com>
2023-01-16 18:50:54 +0100

    Commenti .h

--- include/ExplorationSubmarine.h
+++ include/ExplorationSubmarine.h
@@ -7,15 +7,15 @@
 
 class ExplorationSubmarine: public Ship {
     public:
+        // Costruttore
         ExplorationSubmarine(Coord& front);
-        // controlla se le celle sono vuote, se true allora
-        // cancella quelle vecchie, scrive quelle nuove e modifica i membri della ship
-        void move(Coord& c);
+
+        // Metodi setter
         void set_coord(std::vector<Coord>& coordinates) override;
         void set_coord_from_center(Coord& cord) override;
-    
-    protected:
-        // se trova celle colpite (char minuscoli) stampa 'X', 
-        // se trova celle non danneggiate (char maiuscoli) stampa 'Y'
+        
+        // Muove il sottomarino in posizone c
+        void move(Coord& c);
+        
 };
 #endif /* ExplorationSubmarine_h */
