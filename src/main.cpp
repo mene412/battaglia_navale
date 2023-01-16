84f1427fc682b6fcf1473c26903c9cd4da02a71a
andreamu02 <andrea.mutti@studenti.unipd.it>
2023-01-16 19:41:42 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,11 @@
 int main(int argc, char* argv[]) {
    std::srand(time(NULL));
    if(argc==2){
-       if(!strcmp(argv[1], "pc")){
+       if(argv[1] == "pc"){
            GamePlayer game{};
            game.start();
        }
-       else if(!strcmp(argv[1], "cc")){
+       else if(argv[1] == "cc"){
            GamePc game{};
            game.start();
        }
@@ -29,10 +29,10 @@ int main(int argc, char* argv[]) {
        }
    }
    else if(argc==1)
-       std::cout<<"Command line argument required"<<std::endl;
+       std::cout<<"Command line argument required.\nwrite cc - game computer v computer\nor pc - game player v computer"<<std::endl;
 
    else
-       std::cout<<"Too many command line arguments"<<std::endl;
+       std::cout<<"Too many command line arguments.\nwrite cc - game computer v computer\nor pc - game player v computer"<<std::endl;
     
    return 0;
 }
